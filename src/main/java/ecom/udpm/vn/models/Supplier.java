8f9aeeb8f3ee26435bec8e78ce02f3ddf8cdeb78
ducnv2509 <ducit2509@gmail.com>
2022-09-29 22:14:03 +0700

    created model supplier and staff and config env

--- src/main/java/ecom/udpm/vn/models/Supplier.java
+++ src/main/java/ecom/udpm/vn/models/Supplier.java
@@ -1,37 +1,22 @@
 package ecom.udpm.vn.models;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@Data
 public class Supplier {
-    private static final Logger logger = LogManager.getLogger(Supplier.class);
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nameSupplier;
-    private String dateCreated;
-    private Long accountId;
-    private boolean status;
-
-    public static void main(String[] args) {
-        logger.debug("Debugging log");
-        logger.info("Info log");
-        logger.warn("Hey, This is a warning!");
-        logger.error("Oops! We have an Error. OK");
-        logger.fatal("Damn! Fatal error. Please fix me.1111");
-    }
+    private String name;
+    private String address;
+    private String phone;
+    private Date createdDate;
+    private Date updatedDate;
+    private Long createdBy;
+    private Long updatedBy;
+    private Boolean status;
 }
---
+++ src/main/java/ecom/udpm/vn/service/SupplierService.java
@@ -0,0 +1,9 @@
+package ecom.udpm.vn.service;
+
+import ecom.udpm.vn.models.Supplier;
+
+import java.util.List;
+
+public interface SupplierService {
+    List<Supplier> getAll();
+}
