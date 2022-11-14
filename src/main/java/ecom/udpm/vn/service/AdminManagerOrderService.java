3d47a90e6b5486c28335f6277f3c3876eef443bc
Nguyễn Minh Tứ <minhtu2215@gmail.com>
2022-11-14 20:59:35 +0700

    update order mananger

--- src/main/java/ecom/udpm/vn/service/AdminManagerOrderService.java
+++ src/main/java/ecom/udpm/vn/service/AdminManagerOrderService.java
@@ -1,11 +1,15 @@
 package ecom.udpm.vn.service;
 
+import ecom.udpm.vn.dto.response.OrderPurchaseItem;
 import ecom.udpm.vn.entity.OrderPurchase;
+import ecom.udpm.vn.entity.OrderPurchaseItems;
 
 import java.util.List;
 
 public interface AdminManagerOrderService {
     List<OrderPurchase> showOrderCustomer();
+
+    List<OrderPurchaseItem> showOrderItemByIdOrder(Long idOrder);
 
     void updateMultiOrderCustomer(List<Long> listId, Integer statusId);
 
--- src/main/java/ecom/udpm/vn/service/impl/AdminManagerOrderServiceImpl.java
+++ src/main/java/ecom/udpm/vn/service/impl/AdminManagerOrderServiceImpl.java
@@ -1,22 +1,41 @@
 package ecom.udpm.vn.service.impl;
 
+import ecom.udpm.vn.dto.response.OrderPurchaseItem;
+import ecom.udpm.vn.entity.Cart;
 import ecom.udpm.vn.entity.OrderPurchase;
 import ecom.udpm.vn.exception.StaffException;
 import ecom.udpm.vn.repository.AdminManagerOrderRepo;
+import ecom.udpm.vn.repository.IOderPurchaseItemRepo;
 import ecom.udpm.vn.service.AdminManagerOrderService;
-import org.hibernate.criterion.Order;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class AdminManagerOrderServiceImpl implements AdminManagerOrderService {
-    @Autowired
-    AdminManagerOrderRepo adminManagerOrderRepo;
+    private final AdminManagerOrderRepo adminManagerOrderRepo;
+    private final IOderPurchaseItemRepo oderPurchaseItemRepo;
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public List<OrderPurchase> showOrderCustomer() {
         return this.adminManagerOrderRepo.findAll();
+    }
+
+    @Override
+    public List<OrderPurchaseItem> showOrderItemByIdOrder(Long idOrder) {
+        this.adminManagerOrderRepo.findById(idOrder).orElseThrow(() -> new StaffException(("id not found: " + idOrder)));
+        String query = "select o.id,o.quantity,o.price,o.total_price,p.image,p.name,p.option1,p.option2,p.option3\n" +
+                "from order_purchase_items o \n" +
+                "join product_variant p on o.id_product = p.id\n" +
+                "where o.id_order = ?";
+        List<OrderPurchaseItem> getOrderItemsByIdOrder = (List<OrderPurchaseItem>) jdbcTemplate.query(query, new BeanPropertyRowMapper(OrderPurchaseItem.class), idOrder);
+        return getOrderItemsByIdOrder;
     }
 
     @Override
